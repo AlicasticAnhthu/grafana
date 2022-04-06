e83c96d1d27362bf367d3027d1689d035985ef30
Alicastic <alicastic@Alicastics-MacBook-Pro.local>
2022-04-06 17:44:59 +0700

    test 2
    Signed-off-by: Alicastic <alicastic@Alicastics-MacBook-Pro.local>

---
+++ node_exporter.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Install node_exporter
+version="${VERSION:-0.18.0}"
+arch="${ARCH:-linux-amd64}"
+bin_dir="${BIN_DIR:-/usr/local/bin}"
+
+# Check folder opt
+mkdir -p /opt;
+
+# Download node_exporter
+wget "https://github.com/prometheus/node_exporter/releases/download/v$version/node_exporter-0.18.0.linux-amd64.tar.gz"
