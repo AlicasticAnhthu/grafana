1be9b3d5a124e5a1cad85a95a7eabf1da72e09c5
Alicastic <alicastic@Alicastics-MacBook-Pro.local>
2022-04-07 14:27:48 +0700

    sucessfully installed version
    Signed-off-by: Alicastic <alicastic@Alicastics-MacBook-Pro.local>

--- node_exporter.sh
+++ node_exporter.sh
@@ -8,4 +8,46 @@ bin_dir="${BIN_DIR:-/usr/local/bin}"
 mkdir -p /opt;
 
 # Download node_exporter
-wget "https://github.com/prometheus/node_exporter/releases/download/v$version/node_exporter-$version.linux-amd64.tar.gz"
+wget "https://github.com/prometheus/node_exporter/releases/download/v$version/node_exporter-$version.$arch.tar.gz" \
+    -O /opt/node_exporter.tar.gz
+echo "Downloaded node_exporter v$version"
+
+# Check folder node_exporter
+mkdir -p /opt/node_exporter;
+
+# move node_exporter to /usr/local/bin
+cd /opt
+
+tar xfz /opt/node_exporter.tar.gz -C /opt/node_exporter || { echo "Error extracting node_exporter tar"; exit 1;}
+
+if [ ! -f $bin_dir/node_exporter ]; then
+    cp "/opt/node_exporter/node_exporter-$version.$arch/node_exporter" "$bin_dir";
+fi 
+
+if [ ! -f /etc/systemd/system/node_exporter.service ]; then
+cat <<EOF > /etc/systemd/system/node_exporter.service
+[Unit]
+Description=Prometheus Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOF
+fi
+
+useradd -M -r -s /bin/false node_exporter
+
+chown node_exporter:node_exporter /usr/local/bin/node_exporter
+
+systemctl daemon-reload
+systemctl start node_exporter.service
+systemctl enable node_exporter.service
+
+echo "Node_exporter successfully installed"
